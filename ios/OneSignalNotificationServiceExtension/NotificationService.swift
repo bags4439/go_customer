322a55423ad530f9ad52031602fe606368a6c9bc
edem4439 <bags4439@gmail.com>
2026-08-19 12:40:23 +0000

    ios onesignal fix

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,41 @@
+import OneSignalExtension
+import UserNotifications
+
+final class NotificationService: UNNotificationServiceExtension {
+  private var contentHandler: ((UNNotificationContent) -> Void)?
+  private var receivedRequest: UNNotificationRequest?
+  private var bestAttemptContent: UNMutableNotificationContent?
+
+  override func didReceive(
+    _ request: UNNotificationRequest,
+    withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
+  ) {
+    receivedRequest = request
+    self.contentHandler = contentHandler
+    bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
+
+    guard let bestAttemptContent else {
+      contentHandler(request.content)
+      return
+    }
+
+    OneSignalExtension.didReceiveNotificationExtensionRequest(
+      request,
+      with: bestAttemptContent,
+      withContentHandler: contentHandler
+    )
+  }
+
+  override func serviceExtensionTimeWillExpire() {
+    if let receivedRequest, let bestAttemptContent {
+      OneSignalExtension.serviceExtensionTimeWillExpireRequest(
+        receivedRequest,
+        with: bestAttemptContent
+      )
+    }
+
+    if let contentHandler, let bestAttemptContent {
+      contentHandler(bestAttemptContent)
+    }
+  }
+}
